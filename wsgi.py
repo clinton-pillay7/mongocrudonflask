26592ff18a41a64632b6e41a8c9f3cf5b237d82e
clinton <clinton.pillay@outlook.com>
2023-02-11 13:20:53 +0200

    added mongodb connection string

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,23 @@
 from flask import Flask
 import socket
+import pymongo
+
 
 app = Flask(__name__)
 
+#myclient = pymongo.MongoClient("mongodb://192.168.8.108:27017/")
+myclient = pymongo.MongoClient("mongodb://root:rootpassword@192.168.8.108:27017/?authMechanism=DEFAULT")
+
+dblist = []
+for db in myclient.list_databases():
+	dblist.append(db)
+
 @app.route("/")
 def home():
-    return f"Container ID: {socket.gethostname()}"
-
+#        return f"Container ID: {socket.gethostname()}"
+	 return dblist
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
 
 
