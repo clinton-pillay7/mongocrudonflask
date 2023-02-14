350aeee163f710e7279abcd0251999cc8dc6d40c
clinton <clinton.pillay@outlook.com>
2023-02-14 22:02:26 +0200

    added an API endpoint - to filter and return all "female" gender documents from the mongoDB collection

--- wsgi.py
+++ wsgi.py
@@ -10,20 +10,15 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-#myclient = pymongo.MongoClient("mongodb://192.168.8.108:27017/")
 myclient = pymongo.MongoClient("mongodb://root:rootpassword@192.168.8.108:27017/?authMechanism=DEFAULT")
 
 mydb = myclient.cruddb
 
 mycol = mydb.crudcollection
 
-
-
-
 @app.route("/")
 def home():
 #    return f"Container ID: {socket.gethostname()}"
-#	 return dblist
 	documents = mycol.find()
 	response = []
 	for document in documents:
@@ -32,16 +27,18 @@ def home():
 	return jsonify(response)
 
 @app.route("/api")
-def home():
-#    return f"Container ID: {socket.gethostname()}"
-#        return dblist
-        documents = mycol.find()
-        response = []
-        for document in documents:
-                document["_id"] = str(document["_id"])
-                response.append(document)
-        return jsonify(response)
+def api():
+	#return f"Container ID: {socket.gethostname()}"
+	#gender = request.args.get('gendervar')
+	documents = mycol.find({"gender":"Female"})
+	response = []
+	for document in documents:
+		document["_id"] = str(document["_id"])
+		response.append(document)
+	return jsonify(response)
+
 
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
